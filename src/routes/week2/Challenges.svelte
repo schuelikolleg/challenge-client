368c69e13e67c89ac9daea65b9cafdc67ceb2448
loumoe <57172796+loumoe@users.noreply.github.com>
2024-05-26 17:56:33 +0200

    add some challenges

--- src/routes/week2/Challenges.svelte
+++ src/routes/week2/Challenges.svelte
@@ -1,23 +1,58 @@
 <script lang="ts">
-import Challenge from "../../lib/Challenge.svelte";
+  import Challenge from "../../lib/Challenge.svelte";
 </script>
 
 <main>
-    <section>
-        <h2>Woche 2 - Challenges</h2>
-    </section>
-    <section>
-        <Challenge flag={"picoCTF{p33k_@_b00_d4ca652e}"} name="Scan me!" description="Baum" file="/week2/qr.png" />
+  <section>
+    <h2>Woche 2 - Challenges</h2>
+  </section>
+  <section>
+    <Challenge
+      flag={"picoCTF{p33k_@_b00_d4ca652e}"}
+      name="Scan me!"
+      description="Baum"
+      file="/week2/qr.png"
+    />
 
-        <Challenge flag={"picoCTF{f1u3n7_1n_pn9_&_pdf_53b741d6}"} name="Secret of the polyblob" file="/week2/flag2of2-final.pdf" />
+    <Challenge
+      flag={"picoCTF{f1u3n7_1n_pn9_&_pdf_53b741d6}"}
+      name="Secret of the polyblob"
+      file="/week2/flag2of2-final.pdf"
+    />
 
-        <Challenge flag={"test"} name="Name" description="fdjhgjorklfjvgoifvgroghjorfgo" file="/week2/qr.png" />
+    <Challenge
+      flag={"flag{wow!_data_is_cool}"}
+      name="Minions 1"
+      description="!!!!!!!!!"
+      file="/week2/minions1.jpg"
+    />
 
-    </section>
+    <Challenge
+      flag={"picoCTF{ac0072c423ee13bfc0b166af72e25b61}"}
+      name="Matrjoschka"
+      description="!!!!!!!!!"
+      file="/week2/matrjoschka.png"
+    />
+    <Challenge
+      flag={"flag{never_change_copyright}"}
+      name="Pinguin"
+      description="!!!!!!!!!"
+      file="/week2/pinguin.jpg"
+    />
+    <Challenge
+      flag={"picoCTF{more_than_m33ts_the_3y3657BaB2C}"}
+      name="Garten"
+      description="!!!!!!!!!"
+      file="/week2/garden.jpg"
+    />
+    <Challenge
+      flag={"picoCTF{ME74D47A_HIDD3N_3b9209a2}"}
+      name="64Basen"
+      description="!!!!!!!!!"
+      file="/week2/64basen.jpg"
+    />
+  </section>
 </main>
 
 <style>
-
-
-
 </style>
