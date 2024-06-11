1ed20aab9e7aa2eaafda0c5e8981a12776a59024
loumoe <57172796+loumoe@users.noreply.github.com>
2024-06-11 16:28:33 +0200

    add challenges and files

--- src/routes/week3/Challenges.svelte
+++ src/routes/week3/Challenges.svelte
@@ -24,21 +24,50 @@
       flag={"picoCTF{bra1lle_is_4_c0ol_1nv3nti0n}"}
     />
     <Challenge
+      name="First Greps"
+      description={"Ich glaube, da gab es ein Terminal Command, um in Dateien zu suchen."}
+      flag={"picoCTF{grep_is_good_to_find_things_dba08a45}"}
+      file="/week3/searchMe"
+    />
+    <Challenge
+      name="Where are the robots?"
+      description={"https://jupiter.challenges.picoctf.org/problem/56830/ \n Google, Bing & Co sind nicht in der Lage, diese Flag zu finden 😔"}
+      flag={"picoCTF{ca1cu1at1ng_Mach1n3s_1bb4c}"}
+    />
+    <Challenge
+      name="Tab, Tab, Attack"
+      description="Tab-Completion ist schon eine feine Sache"
+      flag={"picoCTF{l3v3l_up!_t4k3_4_r35t!_76266e38}"}
+      file="/week3/AddadshAddadshashanammu.zip"
+    />
+    <Challenge
+      name="Wave a flag"
+      description={"Eine ganz neue Art der Verschlüsselung! Setze deine Entzifferung am Ende in picoCTF{…}. mlnklfnknljflfjljnjijjmmjkmljnjhmhjgjnjjjmmkjjmijhmkjhjpmkmkmljkjijnjpmhmjjgjj  (TODO fehlt hier die Python Datei?"}
+      flag={"picoCTF{et_tu?_1ac5f3d7920a85610afeb2572831daa8}"}
+    />
+    <Challenge
+      name="Rock 'n' roll"
+      description="Wir haben einen Song geschrieben! Nur war er in seiner Originalsprache nicht sehr erfolgreich, deswegen haben wir ihn übersetzt."
+      flag={"picoCTF{rrrocknrn0113r}"}
+      file="/week3/lyrics.txt"
+    />
+    <Challenge
       name="Tux!"
       description="And I would walk 500 miles..."
       flag={"CTFlearn{Linux_Is_Awesome}"}
       file="/week2/tux.jpg"
     />
     <Challenge
+      name="New Caesar"
+      description={"Eine ganz neue Art der Verschlüsselung! Setze deine Entzifferung am Ende in picoCTF{…}. mlnklfnknljflfjljnjijjmmjkmljnjhmhjgjnjjjmmkjjmijhmkjhjpmkmkmljkjijnjpmhmjjgjj"}
+      flag={"picoCTF{b1scu1ts_4nd_gr4vy_30e77291}"}
+      file="week3/new_caesar.py"
+    />
+    <Challenge
       name="Abandoned Place"
       description="Der Schlüssel liegt in der Auflösung."
       flag={"CTFlearn{urban_exploration}"}
       file="/week2/abandonedStreet.jpg"
-    />
-    <Challenge
-      name="New Caesar"
-      description={"Eine ganz neue Art der Verschlüsselung! Setze deine Entzifferung am Ende in picoCTF{…}. mlnklfnknljflfjljnjijjmmjkmljnjhmhjgjnjjjmmkjjmijhmkjhjpmkmkmljkjijnjpmhmjjgjj  (TODO fehlt hier die Python Datei?"}
-      flag={"picoCTF{et_tu?_1ac5f3d7920a85610afeb2572831daa8}"}
     />
   </section>
 </main>
